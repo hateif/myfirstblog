5f48a7667a92a05ead766c15bcb0a4b8158219e5
lihao <lihao@tdmarco.com>
2016-01-17 21:00:06 +0800

    test

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 #from blog.views import simple_chart
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    #url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.simple_chart, name='post_list'),
     url(r'^simple_chart/$', views.simple_chart, name='simple_chart'),
-    #url(r'^simple_chart/$', views.simple_chart, name="simple_chart"),
 ]
