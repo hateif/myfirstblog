e045f95d7650ccdcb236f5b7da66b12e25d7ffc8
lihao <lihao@tdmarco.com>
2016-01-17 20:23:37 +0800

    bokeh added

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from . import views
+#from blog.views import simple_chart
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^simple_chart/$', views.simple_chart, name='simple_chart'),
+    #url(r'^simple_chart/$', views.simple_chart, name="simple_chart"),
 ]
